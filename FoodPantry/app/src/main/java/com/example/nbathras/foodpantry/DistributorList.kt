298d8adb3a3c9a405f2c3bf1613ad64e1971a6de
Sophia Khezri <Sophiak@Sophias-MacBook-Pro-2.local>
2019-11-04 00:41:31 -0500

    DonorSplashActivity: ListView

---
+++ FoodPantry/app/src/main/java/com/example/nbathras/foodpantry/DistributorList.kt
@@ -0,0 +1,32 @@
+package com.example.nbathras.foodpantry
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+//Customized adapter for DonorSplashActivity
+class DistributorList(private val context:Activity, internal var distributors: List<Distributor>):
+    ArrayAdapter<Distributor>(context,R.layout.donor_splash_list_item, distributors){
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+       //inflating donor_splash_list_item layout
+        val inflater = context.layoutInflater
+        val listViewItem = inflater.inflate(R.layout.donor_splash_list_item, null, true)
+
+        val textViewName = listViewItem.findViewById<View>(R.id.distributorName) as TextView
+        val textViewAbout = listViewItem.findViewById<View>(R.id.distributorAbout) as TextView
+
+        val distributor = distributors[position]
+        //Populating 'name' and 'about' text fields for each list element
+        textViewName.text = distributor.distributorName
+        textViewAbout.text = distributor.distributorAbout
+
+        return listViewItem
+    }
+
+
+}
--- FoodPantry/app/src/main/java/com/example/nbathras/foodpantry/DonorSplashActivity.kt
+++ FoodPantry/app/src/main/java/com/example/nbathras/foodpantry/DonorSplashActivity.kt
@@ -1,18 +1,71 @@
 package com.example.nbathras.foodpantry
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.SharedPreferences
+import android.widget.ListView
+import android.widget.ArrayAdapter
+import android.view.View
+
+import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 
 class DonorSplashActivity : AppCompatActivity() {
+
+    private lateinit var databaseDistributors: DatabaseReference
+    private lateinit var userUID: String
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var distributors: MutableList<Distributor>
+    private lateinit var listViewDistributor: ListView
+    private lateinit var listViewAdapter: ArrayAdapter<Distributor>
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donor_splash)
 
-        // look at the login activity
-        // all of the basic information like whether the user is a donor or distributor as well as
-        // the email, name, address, about are all stored in shared preferences so we do not have to
-        // continue doing gets every time we want to display the user profile
-        // System will probably change in soon when I come up with a better way to store everything
+
+        sharedPreferences = getSharedPreferences(MY_PREFERENCE, Context.MODE_PRIVATE)
+        //Obtaining specific userID through sharedPreferences
+        userUID = sharedPreferences.getString(USER_ID, "Null").toString()
+        //Accessing distributors list from firebase database
+        databaseDistributors = FirebaseDatabase.getInstance().getReference("distributors").child(userUID)
+        //distributors arraylist to be populated with values from databaseDistributors
+        distributors = ArrayList()
+        //List view defined in layout file
+        listViewDistributor = findViewById<View>(R.id.donor_splash_list) as ListView
     }
+
+    override fun onStart() {
+        super.onStart()
+        databaseDistributors.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                //clearing the previous distributors list
+                distributors.clear()
+
+                //populating the distributors array from the database
+                for (postSnapshot in dataSnapshot.children) {
+                    val distributor = postSnapshot.getValue<Distributor>(Distributor::class.java)
+                    //add distributor to the list
+                    distributors.add(distributor!!)
+                }
+
+                //Initializing listViewAdapter to customized DistributorList adapter
+                listViewAdapter = DistributorList(this@DonorSplashActivity, distributors)
+                listViewDistributor.adapter = listViewAdapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                //Empty
+            }
+        })
+    }
+
+    companion object {
+        const val MY_PREFERENCE   = "myPreference"
+        const val USER_ID         = "userID"
+    }
+
 }
