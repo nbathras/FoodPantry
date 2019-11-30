2bf7ac7b198adb095cd481c9538954c44aca3615
Sophia Khezri <Sophiak@Sophias-MBP-2.fios-router.home>
2019-11-30 00:09:33 -0500

    Update request items with new values from donations

--- FoodPantry/app/src/main/java/com/example/nbathras/foodpantry/DonorSubmitRequestActivity.kt
+++ FoodPantry/app/src/main/java/com/example/nbathras/foodpantry/DonorSubmitRequestActivity.kt
@@ -25,6 +25,10 @@ class DonorSubmitRequestActivity : AppCompatActivity() {
     private lateinit var databaseDistributorRequest: DatabaseReference
     private lateinit var userID: String
     private lateinit var finishDate:String
+    private lateinit var requestId: String
+    private lateinit var distributorId: String
+    private  var donationList: ArrayList<Pair<String, Int>> = ArrayList<Pair<String, Int>>()
+    private  var donationHashMap: HashMap<String, Int> = HashMap<String, Int>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,8 +38,8 @@ class DonorSubmitRequestActivity : AppCompatActivity() {
         userID = intent.getStringExtra(DistributorPageRequestActivity.USER_ID)
         finishDate = intent.getStringExtra(DistributorPageRequestActivity.REQUEST_DATE)
 
-        val requestId = intent.getStringExtra(DistributorPageRequestActivity.REQUEST_ID)
-        val distributorId = intent.getStringExtra(DistributorPageRequestActivity.DISTRIBUTOR_ID)
+        requestId = intent.getStringExtra(DistributorPageRequestActivity.REQUEST_ID)
+        distributorId = intent.getStringExtra(DistributorPageRequestActivity.DISTRIBUTOR_ID)
         intent.getStringExtra(DistributorPageRequestActivity.REQUEST_ITEMS)
 
         databaseDonations = FirebaseDatabase.getInstance().getReference("donations").child(userID)
@@ -54,7 +58,10 @@ class DonorSubmitRequestActivity : AppCompatActivity() {
         submitButton.setOnClickListener {
             seekBarValueMap = DonationToDistributorListItem(this@DonorSubmitRequestActivity,
                 requestItemsList).seekBarValues
+            //submit donation from seek bar values
             submitDonation(seekBarValueMap)
+            //update the request list
+            updateRequestList()
         }
 
     }
@@ -65,22 +72,48 @@ class DonorSubmitRequestActivity : AppCompatActivity() {
         donationToDistributorListView.adapter = donationToDistributorListAdapter
         }
 
+    //This function will submit the donation values to the database
     fun submitDonation(seekBarMap:HashMap<String, Int>) {
-        val donationList: ArrayList<Pair<String, Int>> = ArrayList<Pair<String, Int>>()
-        val isDelivered: Boolean = false
+        var isDelivered: Boolean = false
         for((key, value) in seekBarMap){
             if(value != 0) {
+                //Adding donation items to an arraylist (because it is used in Donation object)
                 donationList.add(Pair(key, value))
+                //Adding a donation item to hashmap  (because it is easier to access when updating Request)
+                donationHashMap.put(key, value)
             }
         }
+        //Create donation object
+        val donation = Donation(requestId, userID,finishDate, isDelivered, donationList)
 
-        val randomId = databaseDonations.push().key
-        val donation = Donation(randomId!!, userID,finishDate, isDelivered, donationList)
-
-        databaseDonations.child(randomId).setValue(donation)
+        databaseDonations.child(requestId).setValue(donation)
 
         Toast.makeText(this, "You have successfully submitted a donation!" +
                 "Please arrive on the scheduled day.", Toast.LENGTH_LONG).show()
+    }
+
+    /**This function will be called in order to update the Request item values once a donation has been submitted
+     * ex: if the orginially amount of cereal needed is 10 boxes, it will decrease to 8 once a donation is made for 2 boxes
+     */
+    fun updateRequestList() {
+        //Retrieve request with corresponding requestID
+        var request = (databaseDistributorRequest.child(requestId)) as Request
+        //New request list to be populated with updated values for corresponding request
+        var updatedRequestList: ArrayList<Pair<String, Pair<Int, Int>>> = ArrayList<Pair<String, Pair<Int, Int>>>()
+
+        for((key, valueRequest) in request.itemsList){
+            //Checks if any donations have been made for a particular item in the current request list. If so, update new values in new request list
+            if(donationHashMap.containsKey(key)) {
+                var newNumNeeded = valueRequest.second - donationHashMap.getValue(key)
+                updatedRequestList.add(Pair(key, Pair(0, newNumNeeded)))
+            }
+            //Else, add originally value to updatedRequestList
+            else {
+                updatedRequestList.add(Pair(key, Pair(0, valueRequest.second)))
+            }
+        }
+        var newUpdatedRequest = Request(request.deliveryDate, request.requestId, request.finishDate, updatedRequestList)
+        databaseDistributorRequest.child(requestId).setValue(newUpdatedRequest)
     }
 
     }
